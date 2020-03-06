7119b527bb3704aad39fbbcadba4eb7487f8162e
jay-zhou-ikun <543882743@qq.com>
2020-03-06 22:39:11 +0800

    element-ui alert源码学习

---
+++ vue/element-ui-code/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+   <el-alert
+    title="成功提示的文案"
+    type="success"
+    description="文字说明文字说明文字说明文字说明文字说明文字说明"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="消息提示的文案"
+    type="info"
+    description="文字说明文字说明文字说明文字说明文字说明文字说明"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="警告提示的文案"
+    type="warning"
+    description="文字说明文字说明文字说明文字说明文字说明文字说明"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error"
+    description="文字说明文字说明文字说明文字说明文字说明文字说明"
+    show-icon>
+  </el-alert>
+  </div>
+</template>
+
+<script>
+import ElAlert from './components/ElAlert.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ElAlert
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
