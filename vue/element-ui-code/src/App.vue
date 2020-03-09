b47a4cd3dbf6f4033e1cf35f45f2e628bc163507
jay-zhou-ikun <543882743@qq.com>
2020-03-09 22:31:59 +0800

    走马灯源码学习

--- vue/element-ui-code/src/App.vue
+++ vue/element-ui-code/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-   <el-alert
+    <carousel height="150px">
+      <carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </carousel-item>
+    </carousel>
+    <!-- <carousel-item /> -->
+   <!-- <el-alert
     title="成功提示的文案"
     type="success"
     description="文字说明文字说明文字说明文字说明文字说明文字说明"
@@ -23,28 +29,28 @@
     type="error"
     description="文字说明文字说明文字说明文字说明文字说明文字说明"
     show-icon>
-  </el-alert>
+  </el-alert> -->
   </div>
 </template>
 
 <script>
-import ElAlert from './components/ElAlert.vue'
+// import ElAlert from './components/ElAlert.vue'
 
 export default {
   name: 'App',
   components: {
-    ElAlert
+    // ElAlert
+    // 'carousel': Carousel,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+html, body {
+  background-color: green;
 }
 </style>
--- vue/element-ui-code/src/main.js
+++ vue/element-ui-code/src/main.js
@@ -3,6 +3,32 @@ import App from './App.vue'
 // import EmlementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 // import ElementUI from 'element-ui';
+
+//引入组件 它们想成为全局组件
+import Carousel from './components/carousel/index';
+import CarouselItem from './components/carousel-item/index.js';
+// console.log(Carousel);
+// Carousel.install(Vue)
+// CarouselItem.install(Vue)
+// Vue.component(Carousel.name, Carousel);
+//放到一个数组里
+const components = [
+  Carousel,
+  CarouselItem
+];
+//封装了成为全局组件的细节
+const install = function(Vue) {
+  components.forEach(component => {
+    console.log(component)
+    //Vue.component vue语法中， 可以全局引入组件的api
+    // Vue.component(component.name, component)
+    component.install(Vue)
+  })
+}
+
+//Vue.use  会调用对象的install方法
+install(Vue) //安装一下
+
 Vue.config.productionTip = false
 // Vue.use(ElementUI);
 
