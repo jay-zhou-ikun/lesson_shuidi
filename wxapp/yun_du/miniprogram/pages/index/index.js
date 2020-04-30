93af94478005b6c375c3ed6f12985432afbbceec
jay-zhou-ikun <543882743@qq.com>
2020-04-30 23:06:41 +0800

    云开发

---
+++ wxapp/yun_du/miniprogram/pages/index/index.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    entities: [{
+      image: 'https://img.alicdn.com/tfs/TB1_sLUpeL2gK0jSZFmXXc7iXXa-520-280.jpg_q90_.webp',
+   
+    },
+    {
+      image: 'https://img.alicdn.com/tfs/TB1JxBMoQT2gK0jSZFkXXcIQFXa-520-280.jpg_q90_.webp',
+   
+    },
+    {
+      image: 'https://img30.360buyimg.com/pop/s1180x940_jfs/t1/80324/33/13241/138311/5da83f0bEe5161f83/99307e8368448a64.jpg.webp',
+     
+    },
+    {
+      image: 'https://img10.360buyimg.com/pop/s1180x940_jfs/t1/90590/18/4647/58372/5de8706aEf7ffbf5c/6fbbcf2b58d98230.jpg.webp',
+    
+    },
+    {
+      image: 'https://img12.360buyimg.com/pop/s1180x940_jfs/t1/100646/20/5916/98658/5def765dE1bc0e811/6d4211c8b2e7d7b5.jpg.webp',
+      
+    }]
+  }
+})
