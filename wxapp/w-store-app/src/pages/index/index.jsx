8b54f8f06d73db15691672eebe13c5c22ea83d04
jay-zhou-ikun <543882743@qq.com>
2020-05-05 20:14:54 +0800

    taro

---
+++ wxapp/w-store-app/src/pages/index/index.jsx
@@ -0,0 +1,55 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+
+import { AtButton } from 'taro-ui'
+
+import './index.scss'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+
+    config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <AtButton className='primary' onClick={this.props.add}>+</AtButton>
+        <AtButton className='secondary' onClick={this.props.dec}>-</AtButton>
+        <AtButton className='primary' onClick={this.props.asyncAdd}>async</AtButton>
+        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>Hello, World</Text></View>
+      </View>
+    )
+  }
+}
+
+export default Index
