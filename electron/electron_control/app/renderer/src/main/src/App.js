b0734c441ab3ace05d011dd954c5bd722ae3ac28
jay-zhou-ikun <543882743@qq.com>
2020-06-04 13:37:56 +0800

    x渲染进程

---
+++ electron/electron_control/app/renderer/src/main/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// electron调用 NativeAPI  多进程架构
+// Inter-Process Communication，进程间通信 
+// electron 不支持import  
+// create-react-app  0配置启动 reject 
+
+
+import { ipcRenderer, remote } from 'electron'; // 去到IPC 
+const { Menu, MenuItem } = remote;
+function App() {
+  const [localCode, setLocalCode] = useState('');
+  const [controlText, setControlText] = useState('');
+  const login = async () => {
+    let code = await ipcRenderer.invoke('login');
+    setLocalCode(code)
+  }
+  useEffect(() => {
+    login();
+  }, []);
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+    const menu = new Menu();
+    menu.append(new MenuItem({
+      label: '复制',
+      role: 'copy'
+    }))
+    menu.append(new MenuItem({
+      label: '分享到微信',
+      click: (menuItem, win, keyboardEvent) => {
+        ipcRenderer.send('share-to-wechat', '123123')
+      }
+    }))
+    menu.popup()
+  }
+  return (
+    <div className="App">
+        {/* <h1 onContextMenu={(e) =>handleContextMenu(e)}>Hello World!</h1> */}
+        {
+          controlText ===''?<>
+          <div>你的控制码</div>
+          <span>{localCode}</span>
+          </>:
+          <div>{controlText}</div>
+        }
+    </div>
+  );
+}
+
+
+export default App;
+
