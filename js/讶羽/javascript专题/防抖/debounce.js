43ae698910a80deb4f95e6c50cca3eb5c71bf206
jay-zhou-ikun <543882743@qq.com>
2020-06-07 16:55:29 +0800

    防抖

---
+++ js/讶羽/javascript专题/防抖/debounce.js
@@ -0,0 +1,14 @@
+var count = 1;
+var container = document.getElementById('container');
+
+function getUserAction(e) {
+    container.innerHTML = count++;
+};
+
+var setUseAction = debounce(getUserAction, 10000, true);
+
+container.onmousemove = setUseAction;
+
+document.getElementById("button").addEventListener('click', function(){
+    setUseAction.cancel();
+})
