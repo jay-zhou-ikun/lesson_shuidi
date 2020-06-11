45731bb4593359a0d05e2a39bd06b007e308d8e7
jay-zhou-ikun <543882743@qq.com>
2020-06-11 09:14:35 +0800

    去重3

---
+++ js/讶羽/javascript专题/数组去重/3.js
@@ -0,0 +1,17 @@
+var array = [1, 1, '1'];
+
+function unique(array) {
+    var res = [];
+    var sortedArray = array.concat().sort();
+    var seen;
+    for (var i =0, len = sortedArray.length; i < len; i++) {
+        // 如果是第一个元素或者相邻的元素不相同
+        if (!i || seen !== sortedArray[i]) {
+            res.push(sortedArray[i])
+        }
+        seen = sortedArray[i];
+    }
+    return res;
+}
+
+console.log(unique(array));
