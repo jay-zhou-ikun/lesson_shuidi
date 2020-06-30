e6e6446d39a63e8f5317e3a700c62205577079fb
jay-zhou-ikun <543882743@qq.com>
2020-06-30 12:37:15 +0800

    新特性

---
+++ js/es7-es9/7-includes.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3]
+// pollyfill
+console.log(arr.includes(2));
+
+// 找一个
+// let flag = false;
+// for (let 0 ~ end) {
+//     if () {
+//         flag = true;
+//     }
+// }
---
+++ js/es7-es9/8.object.values.entries.js
@@ -0,0 +1,8 @@
+Object.keys()
+let obj = {
+    a: 1, b: 2
+}
+Object.setPrototypeOf(obj, {c: 3})
+Object.values(obj);
+// Map 
+Object.entries(obj);
